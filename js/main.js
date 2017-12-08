594435c27c48f51627eac0b4162edda498858d20
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-08 12:58:18 -0800

    added check for winner logic

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ for (var i = 0; i < 9; i++) {
 var $boxes = $('.box');
 var count = 0;
 
-// XO functionality
+// click functionality
 $boxes.click(function() {
     if (count % 2 === 0) {
         $(this).text('X');                
@@ -16,8 +16,26 @@ $boxes.click(function() {
         $(this).text('O');          
     }
     count++;
-    $(this).off();  
+    $(this).off();
+    checkWinner();
 });
+
+// check for winner
+function checkWinner() {
+    var row1 = $boxes.eq(0).text() === $boxes.eq(1).text() && $boxes.eq(0).text() === $boxes.eq(2).text() && $boxes.eq(0).text() !== '';
+    var row2 = $boxes.eq(3).text() === $boxes.eq(4).text() && $boxes.eq(3).text() === $boxes.eq(5).text() && $boxes.eq(3).text() !== '';
+    var row3 = $boxes.eq(6).text() === $boxes.eq(7).text() && $boxes.eq(6).text() === $boxes.eq(8).text() && $boxes.eq(6).text() !== '';
+    var col1 = $boxes.eq(0).text() === $boxes.eq(3).text() && $boxes.eq(0).text() === $boxes.eq(6).text() && $boxes.eq(0).text() !== '';
+    var col2 = $boxes.eq(1).text() === $boxes.eq(4).text() && $boxes.eq(1).text() === $boxes.eq(7).text() && $boxes.eq(1).text() !== '';
+    var col3 = $boxes.eq(2).text() === $boxes.eq(5).text() && $boxes.eq(2).text() === $boxes.eq(8).text() && $boxes.eq(2).text() !== '';
+    var dia1 = $boxes.eq(0).text() === $boxes.eq(4).text() && $boxes.eq(0).text() === $boxes.eq(8).text() && $boxes.eq(0).text() !== '';
+    var dia2 = $boxes.eq(2).text() === $boxes.eq(4).text() && $boxes.eq(2).text() === $boxes.eq(6).text() && $boxes.eq(2).text() !== '';
+
+    if (row1 || row2 || row3 || col1 || col2 || col3 || dia1 || dia2) {
+        $($boxes).off();
+        console.log('winner');
+    }
+}
 
 // reset button
 $('button').click(function() {
