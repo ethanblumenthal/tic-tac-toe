779842f7d38ab1765634b188d96d8e7443da2e53
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-08 10:45:54 -0800

    added XO functionality

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@ for (var i = 0; i < 9; i++) {
 }
 
 var $boxes = $('.box');
+var count = 0;
+
+// X/O functionality
+$boxes.click(function() {
+    if (count % 2 === 0) {
+        $(this).text('X');
+        count++;      
+    } else {
+        $(this).text('O');  
+        count++;          
+    }
+});
